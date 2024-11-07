60a593bc57255934ddbe3d45cf76940f476755b3
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2024-11-07 13:54:19 +0100

    .

--- toolbarExample/app/src/main/java/com/maestre/toolbarexample/MainActivity.kt
+++ toolbarExample/app/src/main/java/com/maestre/toolbarexample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.maestre.toolbarexample
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,4 +22,22 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mi_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_search -> {
+                true
+            }
+
+            R.id.menu_info -> {
+                true
+            }
+
+            else -> false
+        }
+    }
 }
