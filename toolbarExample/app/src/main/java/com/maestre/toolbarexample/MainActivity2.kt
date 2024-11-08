f79928103c919929852fd44ca1ac43cd2c5f9462
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2024-11-08 12:45:12 +0100

    .

---
+++ toolbarExample/app/src/main/java/com/maestre/toolbarexample/MainActivity2.kt
@@ -0,0 +1,19 @@
+package com.maestre.toolbarexample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.maestre.toolbarexample.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+    }
+}
